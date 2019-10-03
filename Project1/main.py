1feec4489d51ee0881e33356e39f28aae05d6e17
Lane Henslee <lhenslee@vols.utk.edu>
2019-10-03 18:27:21 -0400

    Read data with pandas

--- Project1/main.py
+++ Project1/main.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-data = pd.read_csv('auto-mgp.data', delim_whitespace=True)
-print(data.head())
+data = pd.read_csv('auto-mpg.data', delim_whitespace=True)
+size = len(data.index)
+training = data[0:int(size*.5)]
+validation = data[int(size*.5)+1:int(size*.8)]
+test = data[int(size*.8)+1:size]
+print(training)
