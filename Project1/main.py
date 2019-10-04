63a86649e6f71eb709797167ce1361a40e289215
Lane Henslee <lhenslee@vols.utk.edu>
2019-10-04 06:17:37 -0400

    Made it much easier to change variables

--- Project1/main.py
+++ Project1/main.py
@@ -40,10 +40,19 @@ def mlr(parameters, x_values):
     return result
 
 
-def get_parameters(data_set):
-    rand_var = data_set.drop(['mpg', 'car name'], axis=1).to_numpy()
+def get_parameters(data_set, x_variables):
+    """
+    Find the parameters for multivariate linear/polynomial regression with the given
+    data_set using the x_variables.
+    :param data_set: Pandas dataframe to learn from.
+    :param x_variables: List of d variable names to learn with.
+    :return: w_0, w_1, ..., w_d
+    """
+    rand_var = pd.DataFrame()
+    for var in x_variables:
+        rand_var[var] = data_set[var]
+    rand_var = rand_var.to_numpy()
     rand_var = np.insert(rand_var, 0, 1, axis=1)
-    #print(rand_var[0])
     results = data_set['mpg'].to_numpy()
     parameters = np.matmul(np.transpose(rand_var), rand_var)
     parameters = np.linalg.inv(parameters)
@@ -52,10 +61,21 @@ def get_parameters(data_set):
     return parameters
 
 
-pars = get_parameters(training)
-for row in training.iterrows():
-    x_vals = [1, row[1]['cylinders'], row[1]['displacement'], row[1]['horsepower'],
-              row[1]['weight'], row[1]['acceleration'], row[1]['model year'], row[1]['origin']]
-    print(row[1]['mpg'], mlr(pars, x_vals))
+def get_estimates(df, parameters, x_variables):
+    estimates = []
+    for row in df.iterrows():
+        x_vals = [1]
+        for var in x_variables:
+            x_vals.append(row[1][var])
+        estimates.append(mlr(parameters, x_vals))
+    return estimates
 
 
+x_vars = ['cylinders', 'displacement', 'horsepower', 'weight', 'acceleration',
+          'model year', 'origin']
+pars = get_parameters(training, x_vars)
+y = get_estimates(training, pars, x_vars)
+plt.plot(training.index, y, label='Estimate')
+plt.plot(training.index, training['mpg'], label='Actual')
+plt.legend()
+plt.show()
